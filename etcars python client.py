9df55974cca30849c6c4f7018bc2e8d89e2b626d
William <71773977+WilliamNT@users.noreply.github.com>
2021-07-18 20:04:24 +0200

    Fixed the code
    Originally I set the packet size to 2048, which is too low. It's now 30.000, and all data can fit through.
    
    There's an issue with validating the JSON raw data currently. I'm working on this issue.

--- etcars python client.py
+++ etcars python client.py
@@ -4,13 +4,13 @@ import time
 def receive_data():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('localhost', 30001))
-    data = s.recv(2048)
+    data = s.recv(30000)
     print(repr(data))
 
 
 while True:
-    time.sleep(5) # Delaying new requests to avoid crashing the game
+    time.sleep(2) # Delaying new requests to avoid crashing the game
     receive_data()
     print('''
-    Updating in 5 seconds (to avoid crashing the game)
+    Updating in 2 seconds (to avoid crashing the game)
     ''')
